92cc49b8bcf16d8cda4774b3ac8bf785a8ff0bf7
Aditi33 <aditikulkarni1392@gmail.com>
2017-12-01 13:50:32 +0000

    Add ASP .Net

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "This is response from HomeController!";
+        }
+    }
+}
--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication
 {
@@ -17,23 +19,82 @@ namespace WebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
                               IHostingEnvironment env,
-                              IGreeter greeter)
+                              IGreeter greeter,
+                              ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            #region Middlewear setup
+
+            //app.Use(next =>
+            //    {
+            //        // Once per http request, middlewear setup
+            //        return async context =>
+            //        {
+            //            logger.LogInformation("Request incoming");
+            //            if (context.Request.Path.StartsWithSegments("/mym"))
+            //            {
+            //                await context.Response.WriteAsync("Hit!");
+            //                logger.LogInformation("Request Handled");
+            //            }
+            //            else
+            //            {
+            //                await next(context);
+            //                logger.LogInformation("Response ongoing");
+            //            }
+            //        };
+            //    }
+            //);
+
+            #endregion
+
+            #region Static file display
+
+            //// Respond with index.html as default
+            //app.UseDefaultFiles();
+
+            // Gives wwwroot\index.html response after browsing index.html page
+            app.UseStaticFiles();
+
+            #endregion
+
+            //// Take case of UseDefaultFiles & UseStaticFiles
+            //app.UseFileServer();
+
+            //app.UseMvcWithDefaultRoute();
+
+            app.UseMvc(ConfigureRoutes);
+
+            app.UseWelcomePage(new WelcomePageOptions
+            {
+                Path = "/wp"
+            });
+
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetWelcomeMessage();
-                await context.Response.WriteAsync(greeting);
+                await context.Response.WriteAsync($"Not Found");
+                //await context.Response.WriteAsync($"{greeting}-{env.EnvironmentName}");
             });
+        }
+
+        private void ConfigureRoutes(IRouteBuilder routeBuilder)
+        {
+            //routeBuilder.MapRoute("Default", "{controller}/{action}");
+
+            // id is optional
+            //routeBuilder.MapRoute("Default", "{controller}/{action}/{id?}");
+
+            routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}");
         }
     }
 }
